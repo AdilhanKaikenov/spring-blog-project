2d4b49b85731ba55e4d368971c0ef63d3030dfe5
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-12-06 13:33:13 +0600

    RootApplicationContextConfiguration.java update

--- blog-web/src/main/java/com/epam/adok/web/configuration/RootApplicationContextConfiguration.java
+++ blog-web/src/main/java/com/epam/adok/web/configuration/RootApplicationContextConfiguration.java
@@ -1,14 +1,73 @@
 package com.epam.adok.web.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScans(value = {
         @ComponentScan("com.epam.adok.core"),
         @ComponentScan("com.epam.adok.web")
 })
+@EnableTransactionManagement
+@EnableJpaRepositories("com.epam.adok.core.dao")
 public class RootApplicationContextConfiguration {
 
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/blog_db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean  entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+        lef.setDataSource(dataSource());
+        lef.setPackagesToScan("com.epam.adok.core");
+        lef.setJpaProperties(additionalProperties());
+
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        lef.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+
+        lef.setPersistenceUnitName("mySqlPU");
+
+        return lef;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.id.new_generator_mappings", "true");
+        return properties;
+    }
+
+    @Bean
+    public EntityManager entityManager() {
+        return entityManagerFactory().getObject().createEntityManager();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        transactionManager.setDataSource(dataSource());
+        return transactionManager;
+    }
 }
