7b37ca2edc17eea8a3295b584d46052099c18ccc
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-12-08 12:48:44 +0600

    Update

--- blog-web/src/main/java/com/epam/adok/web/controller/SpringMvcController.java
+++ blog-web/src/main/java/com/epam/adok/web/controller/SpringMvcController.java
@@ -1,8 +1,16 @@
 package com.epam.adok.web.controller;
 
+import com.epam.adok.core.dao.impl.blog.BlogFilter;
 import com.epam.adok.core.entity.Blog;
+import com.epam.adok.core.entity.Category;
+import com.epam.adok.core.exception.DateParsingException;
 import com.epam.adok.core.service.BlogService;
+import com.epam.adok.core.service.CategoryService;
+import com.epam.adok.core.util.DateRange;
+import com.epam.adok.core.util.DateUtil;
 import com.epam.adok.web.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -10,21 +18,30 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.NoResultException;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @Controller
 public class SpringMvcController {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringMvcController.class);
+
     @Autowired
     private BlogService blogService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @RequestMapping(value = "/blog/", method = RequestMethod.GET)
     public ModelAndView blogs() {
 
         List<Blog> blogs = blogService.findAllBlogs();
 
+        List<Category> categories = categoryService.findAllCategories();
+
         ModelAndView model = new ModelAndView();
         model.addObject("blogs", blogs);
+        model.addObject("categories", categories);
         model.setViewName("blogs");
 
         return model;
@@ -42,6 +59,46 @@ public class SpringMvcController {
         ModelAndView model = new ModelAndView();
         model.addObject("blog", blog);
         model.setViewName("blog");
+
+        return model;
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.POST)
+    public ModelAndView filter(HttpServletRequest request) throws DateParsingException {
+        ModelAndView model = new ModelAndView();
+        String from = request.getParameter("from");
+        String to = request.getParameter("to");
+        String[] categoryLists = request.getParameterValues("categoryList");
+
+        Date dateFrom = null;
+        if (!from.equals("")) {
+            dateFrom = DateUtil.parseStringToDate(from);
+        }
+
+        Date dateTo = null;
+        if (!to.equals("")) {
+            dateTo = DateUtil.parseStringToDate(to);
+        }
+
+        Set<Category> categories = new HashSet<>();
+
+        if (categoryLists != null) {
+            for (String categoryID : categoryLists) {
+                categories.add(categoryService.findCategoryByID(Integer.parseInt(categoryID)));
+            }
+        }
+
+        BlogFilter blogFilter = new BlogFilter();
+        blogFilter.setDateRange(new DateRange(dateFrom, dateTo));
+        blogFilter.setCategories(categories);
+
+        List<Blog> blogs = blogService.findAllBlogsByParameters(blogFilter);
+        List<Category> allCategories = categoryService.findAllCategories();
+
+        model.addObject("blogs", blogs);
+        model.addObject("categories", allCategories);
+
+        model.setViewName("blogs");
 
         return model;
     }
--- blog-web/src/main/webapp/WEB-INF/pages/blogs.jsp
+++ blog-web/src/main/webapp/WEB-INF/pages/blogs.jsp
@@ -6,6 +6,28 @@
     <title>Blogs</title>
 </head>
 <body>
+<table align="center">
+    <tr>
+        <form method="post" action="${pageContext.request.contextPath}/filter">
+            <td>
+                <input type="date" name="from">
+            </td>
+            <td>
+                <input type="date" name="to">
+            </td>
+            <td>
+                <%--@elvariable id="categories" type="java.util.List"--%>
+                <c:forEach items="${categories}" var="category">
+                    <%--@elvariable id="category" type="com.epam.adok.core.entity.Category"--%>
+                    <br><input type="checkbox" name="categoryList" value="${category.id}">${category.genre}
+                </c:forEach>
+            </td>
+            <td>
+                <button type="submit">Filter</button>
+            </td>
+        </form>
+    </tr>
+</table>
 <table border="1" cellpadding="2" align="center">
     <tr bgcolor="#8fbc8f">
         <th>
