d7c0511305dba5b2ce0fe3ad9247ce270f7069c5
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-12-11 12:15:08 +0600

    DateRange.java @DateTimeFormat annotation added

--- blog-core/src/main/java/com/epam/adok/core/util/DateRange.java
+++ blog-core/src/main/java/com/epam/adok/core/util/DateRange.java
@@ -1,11 +1,15 @@
 package com.epam.adok.core.util;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 public class DateRange {
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date from;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private Date to;
 
     public DateRange(Date from, Date to) {
