0fa99e21d4079e9773d78b8d2438cc9b98fd508d
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2017-12-12 15:12:40 +0600

    GenericDao.java fixed

--- blog-core/src/main/java/com/epam/adok/core/dao/impl/GenericDao.java
+++ blog-core/src/main/java/com/epam/adok/core/dao/impl/GenericDao.java
@@ -5,15 +5,17 @@ import com.epam.adok.core.entity.AbstractBaseEntity;
 import com.mysema.query.jpa.impl.JPAQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public abstract class GenericDao<T extends AbstractBaseEntity> implements Dao<T> {
 
-    @Autowired
+    @PersistenceContext(unitName = "mySqlPU")
     private EntityManager entityManager;
 
     public JPAQuery getJpaQuery() {
@@ -24,6 +26,7 @@ public abstract class GenericDao<T extends AbstractBaseEntity> implements Dao<T>
         return entityManager;
     }
 
+    @Transactional
     @Override
     public void save(T t) {
         entityManager.persist(t);
