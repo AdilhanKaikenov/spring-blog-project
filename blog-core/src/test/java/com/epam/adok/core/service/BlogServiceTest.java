da5d80146f53c0b59d647378c3bd65bee7dc6db7
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-01-10 18:47:27 +0600

    Update

--- blog-core/src/test/java/com/epam/adok/core/service/BlogServiceTest.java
+++ blog-core/src/test/java/com/epam/adok/core/service/BlogServiceTest.java
@@ -1,18 +1,39 @@
 package com.epam.adok.core.service;
 
 
-import com.epam.adok.core.configuration.RootApplicationContextConfiguration;
+import com.epam.adok.core.configuration.TestApplicationContextConfiguration;
 import com.epam.adok.core.entity.Blog;
+import com.epam.adok.core.entity.Category;
+import com.epam.adok.core.entity.User;
+import com.epam.adok.core.entity.comment.BlogComment;
+import com.epam.adok.core.exception.BlogNotFoundException;
+import com.epam.adok.core.repository.BlogCommentRepository;
 import com.epam.adok.core.repository.BlogRepository;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RootApplicationContextConfiguration.class)
+@ContextConfiguration(classes = TestApplicationContextConfiguration.class)
 public class BlogServiceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(BlogServiceTest.class);
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Autowired
     private BlogService blogService;
@@ -20,25 +41,52 @@ public class BlogServiceTest {
     @Autowired
     private BlogRepository blogRepository;
 
+    @Autowired
+    private BlogCommentRepository blogCommentRepository;
 
-    public void removeBlogByID_WithExistingBlog_ShouldDeleteBlog() {
+    @Autowired
+    private CommentService<BlogComment> blogCommentService;
+
+    @Test
+    public void removeBlogByID_WithExistingBlog_ShouldDeleteBlog() throws BlogNotFoundException {
+
         // Given
-        Blog blog = new Blog();
-        this.blogRepository.saveAndFlush(blog);
+        User user = new User();
+        user.setId(1);
+
+        Category category = new Category();
+        category.setId(2);
+
+        Set<Category> categories = new HashSet<>();
+        categories.add(category);
+
+        Blog blog = new Blog(
+                "Title", "Content Text", user, categories, new Date());
+
+        Blog savedBlog = this.blogRepository.saveAndFlush(blog);
+
+        BlogComment blogComment = new BlogComment(
+                user, "Blog Comment text", new Date(), savedBlog, null);
+
+        BlogComment submittedBlogComment = blogCommentRepository.saveAndFlush(blogComment);
 
         // When
-//        boolean result = this.blogService.removeBlogByID(0L);
+        long savedBlogId = savedBlog.getId();
+        this.blogService.removeBlogByID(savedBlogId);
 
         // Then
+        long submittedBlogCommentId = submittedBlogComment.getId();
+        Optional<Blog> targetBlog = blogRepository.findById(savedBlogId);
+        Optional<BlogComment> targetComment = blogCommentRepository.findById(submittedBlogCommentId);
+
+        Assert.assertFalse(targetBlog.isPresent());
+        Assert.assertFalse(targetComment.isPresent());
+
     }
 
-    public void removeBlogByID_WithNonExistingBlog_ShouldThrowException() {
-        // Given
-
-        // When
-//        boolean result = this.blogService.removeBlogByID(-125L);
-
-        // Then
-
+    @Test
+    public void removeBlogByID_WithNonExistingBlog_ShouldThrowException() throws BlogNotFoundException {
+        thrown.expect(BlogNotFoundException.class);
+        this.blogService.removeBlogByID(70L);
     }
 }
