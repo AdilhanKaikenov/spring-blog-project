4553aa64a423bec2d570e73b1fbead9a805f6811
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-01-10 18:46:19 +0600

    BlogNotFoundException.java

---
+++ blog-core/src/main/java/com/epam/adok/core/exception/BlogNotFoundException.java
@@ -0,0 +1,12 @@
+package com.epam.adok.core.exception;
+
+public class BlogNotFoundException extends Exception {
+
+    public BlogNotFoundException(String message) {
+        super(message);
+    }
+
+    public BlogNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
