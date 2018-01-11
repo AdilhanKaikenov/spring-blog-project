b84ff163d4770af5d99e7abbe402641e7c611f44
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-01-11 14:14:55 +0600

    BlogServiceUnitTest.java (initial)

---
+++ blog-core/src/test/java/com/epam/adok/core/service/BlogServiceUnitTest.java
@@ -0,0 +1,14 @@
+package com.epam.adok.core.service;
+
+import com.epam.adok.core.configuration.TestApplicationContextConfiguration;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = TestApplicationContextConfiguration.class)
+public class BlogServiceUnitTest {
+
+
+
+}
