56295f7bf6ae0c9e1a6f6b485393b86e5369023c
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-02-02 16:26:25 +0600

    AuthenticationController.java created

---
+++ blog-web/src/main/java/com/epam/adok/web/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.epam.adok.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AuthenticationController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView loginPage(@RequestParam(value = "error",required = false) String error,
+                                  @RequestParam(value = "logout",	required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("message", "Logged out successfully.");
+        }
+
+        model.setViewName("loginPage");
+        return model;
+    }
+}
