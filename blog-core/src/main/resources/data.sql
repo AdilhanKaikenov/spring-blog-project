75262b9fcf4999115d83d2834713fb1146e104f8
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-02-02 16:19:59 +0600

    schema.sql & data.sql updated (user roles)

--- blog-core/src/main/resources/data.sql
+++ blog-core/src/main/resources/data.sql
@@ -17,3 +17,10 @@ INSERT INTO category(id, genre, added_date) VALUES (3, 'Advertisement', '2017-10
 INSERT INTO blog_category_assignment(blog_id, category_id, date) VALUES (1, 2, '2017-10-31 16:40:31');
 INSERT INTO blog_category_assignment(blog_id, category_id, date) VALUES (1, 3, '2017-10-31 16:40:31');
 INSERT INTO blog_category_assignment(blog_id, category_id, date) VALUES (4, 2, '2017-10-31 16:40:31');
+
+INSERT INTO ROLES (ID, NAME) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO ROLES (ID, NAME) VALUES (2, 'ROLE_USER');
+
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_roles (user_id, role_id) VALUES (1, 2);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
--- blog-core/src/main/resources/schema.sql
+++ blog-core/src/main/resources/schema.sql
@@ -3,7 +3,9 @@ DROP TABLE IF EXISTS comment;
 DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS notification;
 DROP TABLE IF EXISTS blog;
+DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS unique_id;
 
 CREATE TABLE `user` (
@@ -18,6 +20,18 @@ CREATE TABLE `user` (
 CREATE TABLE `unique_id` (
   id int NOT NULL AUTO_INCREMENT UNIQUE,
   PRIMARY KEY (id)
+);
+
+CREATE TABLE `roles` (
+  id int NOT NULL AUTO_INCREMENT UNIQUE,
+  PRIMARY KEY (id),
+  `name` VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE `user_roles` (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  CONSTRAINT user_roles_assignment_id_pk PRIMARY KEY (user_id, role_id)
 );
 
 CREATE TABLE `blog` (
@@ -68,6 +82,8 @@ ALTER TABLE `blog` ADD CONSTRAINT `blog_fk1` FOREIGN KEY (`id`) REFERENCES `uniq
 ALTER TABLE `category` ADD CONSTRAINT `category_fk0` FOREIGN KEY (`id`) REFERENCES `unique_id`(`id`);
 ALTER TABLE `blog_category_assignment` ADD CONSTRAINT `blog_category_assignment_fk0` FOREIGN KEY (`blog_id`) REFERENCES `blog`(`id`);
 ALTER TABLE `blog_category_assignment` ADD CONSTRAINT `blog_category_assignment_fk1` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`);
+ALTER TABLE `user_roles` ADD CONSTRAINT `user_roles_fk0` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
+ALTER TABLE `user_roles` ADD CONSTRAINT `user_roles_fk1` FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`);
 ALTER TABLE `comment` ADD CONSTRAINT `comment_fk0` FOREIGN KEY (`blog_id`) REFERENCES `blog`(`id`);
 ALTER TABLE `comment` ADD CONSTRAINT `comment_fk1` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`);
 ALTER TABLE `comment` ADD CONSTRAINT `comment_fk2` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment`(`id`);
