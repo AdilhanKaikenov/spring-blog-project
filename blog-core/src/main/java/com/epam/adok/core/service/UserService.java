02fc9756f4425e7a40e034287841a942d8e0d273
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-02-07 16:35:59 +0600

    UserService.java updated

--- blog-core/src/main/java/com/epam/adok/core/service/UserService.java
+++ blog-core/src/main/java/com/epam/adok/core/service/UserService.java
@@ -1,22 +1,30 @@
 package com.epam.adok.core.service;
 
+import com.epam.adok.core.auth.UserPrincipal;
 import com.epam.adok.core.dao.UserDao;
 import com.epam.adok.core.entity.User;
 import com.epam.adok.core.exception.NotFndException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserDao userDao;
 
     public User getUserByAuth(String login, String password) {
         return userDao.findByAuth(login, password);
+    }
+
+    public User getUserByLogin(String login) {
+        return userDao.findByLogin(login);
     }
 
     public User getCurrentUser() {
@@ -27,15 +35,12 @@ public class UserService {
         }
 
         Object obj = auth.getPrincipal();
-        String login = "";
+        log.info("Principal ---------- > {}", obj);
 
-        if (obj instanceof UserDetails) {
-            login = ((UserDetails) obj).getUsername();
+        if (obj instanceof UserPrincipal) {
+            return userDao.findByLogin(((UserPrincipal) obj).getUsername());
         } else {
-            login = obj.toString();
+            return null; // anonymous user
         }
-
-        User u = userDao.findByLogin(login);
-        return u;
     }
 }
