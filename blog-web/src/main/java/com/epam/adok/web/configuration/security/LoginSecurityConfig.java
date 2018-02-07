99ebf785812bc0246f69d99297cfdfc49cbcaf43
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-02-07 16:37:08 +0600

    Spring Security configuration updated

--- blog-web/src/main/java/com/epam/adok/web/configuration/security/LoginSecurityConfig.java
+++ blog-web/src/main/java/com/epam/adok/web/configuration/security/LoginSecurityConfig.java
@@ -1,14 +1,20 @@
 package com.epam.adok.web.configuration.security;
 
-import com.epam.adok.core.auth.UserAuthenticationProviderService;
+import com.epam.adok.core.auth.AppUserDetailsService;
 import com.epam.adok.core.configuration.MethodSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -17,11 +23,35 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserAuthenticationProviderService authenticationProviderService;
+    private AppUserDetailsService userDetailsService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProviderService);
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return userDetailsService;
     }
 
     @Override
