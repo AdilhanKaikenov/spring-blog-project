130c66a864d4610bf9586a089db5449381f94b99
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-03-02 17:26:26 +0600

    BlogRepository.java minor

--- blog-core/src/main/java/com/epam/adok/core/repository/BlogRepository.java
+++ blog-core/src/main/java/com/epam/adok/core/repository/BlogRepository.java
@@ -1,11 +1,14 @@
 package com.epam.adok.core.repository;
 
 import com.epam.adok.core.entity.Blog;
+import com.epam.adok.core.entity.User;
 import com.epam.adok.core.vo.BlogTitleVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
@@ -14,4 +17,6 @@ public interface BlogRepository extends JpaRepository<Blog, Long> {
     BlogTitleVO getBlogByTitle(@Param("title") String title);
 
     Blog findByTitle(String title);
+
+    Stream<Blog> findByAuthor(User author);
 }
