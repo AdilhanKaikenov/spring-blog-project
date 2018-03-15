c7132cde7f38c0e22225fe0181c9e897af57e6f5
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-03-15 16:47:57 +0600

    SorterByBlogCommentDate.java updated

--- blog-core/src/main/java/com/epam/adok/core/comparator/SorterByBlogCommentDate.java
+++ blog-core/src/main/java/com/epam/adok/core/comparator/SorterByBlogCommentDate.java
@@ -1,18 +1,18 @@
 package com.epam.adok.core.comparator;
 
-import com.epam.adok.core.commentshierarchy.CommentBranch;
+import com.epam.adok.core.entity.comment.BlogComment;
 
 import java.util.Comparator;
 import java.util.Date;
 
-public class SorterByBlogCommentDate implements Comparator<CommentBranch> {
+public class SorterByBlogCommentDate implements Comparator<BlogComment> {
 
     @Override
-    public int compare(CommentBranch commentBranchOne, CommentBranch commentBranchTwo) {
+    public int compare(BlogComment blogCommentOne, BlogComment blogCommentTwo) {
 
-        Date commentDateOne = commentBranchOne.getRootBlogComment().getCommentDate();
-        Date commentDateTwo = commentBranchTwo.getRootBlogComment().getCommentDate();
+        Date commentDateOne = blogCommentOne.getCommentDate();
+        Date commentDateTwo = blogCommentTwo.getCommentDate();
 
-        return commentDateOne.compareTo(commentDateTwo);
+        return commentDateTwo.compareTo(commentDateOne);
     }
 }
