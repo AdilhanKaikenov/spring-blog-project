a5c4ff4320091bc6001baed0979af71b40a01ad6
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-03-15 18:17:54 +0600

    BlogCommentRepository.java updated

--- blog-core/src/main/java/com/epam/adok/core/repository/BlogCommentRepository.java
+++ blog-core/src/main/java/com/epam/adok/core/repository/BlogCommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,8 +18,8 @@ public interface BlogCommentRepository extends JpaRepository<BlogComment, Long> 
 
     int countByBlog(Blog blog);
 
-    @Query("SELECT comment FROM BlogComment comment WHERE comment.parentComment.id IS NULL ORDER BY comment.commentDate DESC")
-    Page<BlogComment> findAllRootBlogCommentsByBlog(Blog blog, Pageable pageable);
+    @Query("SELECT comment FROM BlogComment comment WHERE comment.blog.id = :blogId AND comment.parentComment.id IS NULL ORDER BY comment.commentDate DESC")
+    Page<BlogComment> findAllRootBlogCommentsByBlog(@Param("blogId") long blogId, Pageable pageable);
 
     List<BlogComment> findAllInclusiveByBlogAndParentCommentIdIn(Blog blog, List<Long> parentsIdList, Pageable pageable);
 
