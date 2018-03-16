6afe5438695860a9c7ec2ade073cb36802a9295b
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-03-16 13:53:01 +0600

    GivenTestDataDistributor.java created and CommentServiceTest.java added tests and updated

--- blog-core/src/test/java/com/epam/adok/core/service/CommentServiceTest.java
+++ blog-core/src/test/java/com/epam/adok/core/service/CommentServiceTest.java
@@ -6,13 +6,8 @@ import com.epam.adok.core.dao.CommentDao;
 import com.epam.adok.core.entity.Blog;
 import com.epam.adok.core.entity.User;
 import com.epam.adok.core.entity.comment.BlogComment;
-import com.epam.adok.core.repository.BlogCommentRepository;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import com.ninja_squad.dbsetup.DbSetup;
-import com.ninja_squad.dbsetup.destination.DataSourceDestination;
-import com.ninja_squad.dbsetup.operation.Operation;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +15,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static com.epam.adok.core.service.BlogCommentsStructureBuilder.buildCommentBranchTree;
-import static com.ninja_squad.dbsetup.Operations.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -46,75 +35,11 @@ public class CommentServiceTest {
     @Autowired
     private CommentDao<BlogComment> commentDao;
 
-    @Autowired
-    private BlogCommentRepository blogCommentRepository;
-
-    @Before
-    public void prepareTestData() throws ParseException {
-
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-        Operation operation = sequenceOf(
-                deleteAllFrom("blog_category_assignment", "comment", "blog"),
-                insertInto("blog")
-                        .columns("id", "title", "content", "user_id", "publication_date")
-                        .values(1L, "Blog#1", "Content Text", 1L, "2017-10-31 16:40:31")
-                        .values(4L, "Blog#2", "Content Text", 1L, "2017-10-31 16:40:31")
-                        .build(),
-                insertInto("comment")
-                        .columns("id", "blog_id", "parent_comment_id", "user_id", "text", "comment_date", "comment_type")
-                        .values(1L, 1L, null, 1L, "BlogComment Text id = 1", formatter.parse("2017-10-31 13:00:00"), "BT")
-                        .values(2L, 1L, 1L, 2L, "BlogComment Text id = 2", formatter.parse("2017-10-31 13:30:00"), "BT")
-                        .values(3L, 1L, 1L, 2L, "BlogComment Text id = 3", formatter.parse("2017-10-31 13:40:00"), "BT")
-                        .values(4L, 1L, 3L, 1L, "BlogComment Text id = 4", formatter.parse("2017-10-31 13:45:00"), "BT")
-                        .values(5L, 1L, 1L, 2L, "BlogComment Text id = 5", formatter.parse("2017-10-31 13:50:00"), "BT")
-                        .values(6L, 1L, 5L, 1L, "BlogComment Text id = 6", formatter.parse("2017-10-31 14:00:00"), "BT")
-                        .values(7L, 1L, 5L, 1L, "BlogComment Text id = 7", formatter.parse("2017-10-31 14:15:00"), "BT")
-                        .values(8L, 1L, 5L, 1L, "BlogComment Text id = 8", formatter.parse("2017-10-31 14:20:00"), "BT")
-                        .values(9L, 1L, 5L, 1L, "BlogComment Text id = 9", formatter.parse("2017-10-31 14:40:00"), "BT")
-                        .values(10L, 1L, 3L, 1L, "BlogComment Text id = 10", formatter.parse("2017-10-31 14:55:00"), "BT")
-                        .values(11L, 1L, 2L, 2L, "BlogComment Text id = 11", formatter.parse("2017-10-31 15:10:00"), "BT")
-                        .values(12L, 1L, 1L, 1L, "BlogComment Text id = 12", formatter.parse("2017-10-31 15:40:00"), "BT")
-                        .values(13L, 1L, 5L, 2L, "BlogComment Text id = 13", formatter.parse("2017-10-31 16:40:00"), "BT")
-                        .values(14L, 1L, 6L, 1L, "BlogComment Text id = 14", formatter.parse("2017-10-31 16:50:00"), "BT")
-                        .values(15L, 1L, 7L, 2L, "BlogComment Text id = 15", formatter.parse("2017-10-31 17:40:00"), "BT")
-                        .values(16L, 1L, 3L, 1L, "BlogComment Text id = 16", formatter.parse("2017-10-31 17:50:00"), "BT")
-                        .values(17L, 1L, 2L, 2L, "BlogComment Text id = 17", formatter.parse("2017-10-31 18:20:00"), "BT")
-                        .values(18L, 1L, 3L, 1L, "BlogComment Text id = 18", formatter.parse("2017-10-31 18:25:00"), "BT")
-                        .values(19L, 1L, 4L, 2L, "BlogComment Text id = 19", formatter.parse("2017-10-31 19:00:00"), "BT")
-                        .values(20L, 1L, 6L, 1L, "BlogComment Text id = 20", formatter.parse("2017-10-31 19:20:00"), "BT")
-                        .values(21L, 1L, 1L, 2L, "BlogComment Text id = 21", formatter.parse("2017-10-31 19:30:00"), "BT")
-                        .values(22L, 1L, 2L, 1L, "BlogComment Text id = 22", formatter.parse("2017-10-31 19:40:00"), "BT")
-                        .values(23L, 1L, null, 1L, "BlogComment Text id = 23", formatter.parse("2017-10-31 13:20:00"), "BT")
-                        .values(24L, 1L, 23L, 2L, "BlogComment Text id = 24", formatter.parse("2017-10-31 13:35:00"), "BT")
-                        .values(25L, 1L, 24L, 2L, "BlogComment Text id = 25", formatter.parse("2017-10-31 19:45:00"), "BT")
-                        .values(26L, 1L, 24L, 1L, "BlogComment Text id = 26", formatter.parse("2017-10-31 19:45:00"), "BT")
-                        .values(27L, 1L, 23L, 2L, "BlogComment Text id = 27", formatter.parse("2017-10-31 13:55:00"), "BT")
-                        .values(28L, 1L, 26L, 1L, "BlogComment Text id = 28", formatter.parse("2017-10-31 14:30:00"), "BT")
-                        .values(29L, 1L, 27L, 1L, "BlogComment Text id = 29", formatter.parse("2017-10-31 19:20:00"), "BT")
-                        .values(30L, 1L, 29L, 1L, "BlogComment Text id = 30", formatter.parse("2017-10-31 14:20:50"), "BT")
-                        .values(31L, 1L, 28L, 1L, "BlogComment Text id = 31", formatter.parse("2017-10-31 14:40:40"), "BT")
-                        .values(32L, 1L, 26L, 1L, "BlogComment Text id = 32", formatter.parse("2017-10-31 14:55:20"), "BT")
-                        .values(33L, 1L, 29L, 2L, "BlogComment Text id = 33", formatter.parse("2017-10-31 15:10:30"), "BT")
-                        .values(34L, 1L, 30L, 1L, "BlogComment Text id = 34", formatter.parse("2017-10-31 15:40:50"), "BT")
-                        .values(35L, 1L, 31L, 2L, "BlogComment Text id = 35", formatter.parse("2017-10-31 16:40:30"), "BT")
-                        .values(36L, 1L, 33L, 1L, "BlogComment Text id = 36", formatter.parse("2017-10-31 16:50:20"), "BT")
-                        .values(37L, 1L, 33L, 2L, "BlogComment Text id = 37", formatter.parse("2017-10-31 17:40:30"), "BT")
-                        .values(38L, 1L, 25L, 1L, "BlogComment Text id = 38", formatter.parse("2017-10-31 17:50:40"), "BT")
-                        .values(39L, 1L, 26L, 2L, "BlogComment Text id = 39", formatter.parse("2017-10-31 18:20:50"), "BT")
-                        .values(40L, 1L, 32L, 1L, "BlogComment Text id = 40", formatter.parse("2017-10-31 18:25:30"), "BT")
-                        .values(41L, 1L, 33L, 2L, "BlogComment Text id = 41", formatter.parse("2017-10-31 19:00:50"), "BT")
-                        .values(42L, 1L, 31L, 1L, "BlogComment Text id = 42", formatter.parse("2017-10-31 19:20:30"), "BT")
-                        .values(43L, 1L, 34L, 2L, "BlogComment Text id = 43", formatter.parse("2017-10-31 19:30:50"), "BT")
-                        .values(44L, 1L, 37L, 1L, "BlogComment Text id = 44", formatter.parse("2017-10-31 19:45:20"), "BT")
-                        .build()
-        );
-        DbSetup dbSetup = new DbSetup(new DataSourceDestination(this.dataSource), operation);
-        dbSetup.launch();
-    }
-
     @Test
     public void findAllCommentsByBlogID_ExistingBlogWithSeveralComments_ShouldReturnAllCommentsOfSpecificBlog() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+
         // When
         List<BlogComment> allBlogCommentByBlogId = this.commentService.findAllBlogCommentByBlogId(1L);
         int numberOfComments = allBlogCommentByBlogId.size();
@@ -125,6 +50,9 @@ public class CommentServiceTest {
 
     @Test
     public void buildAllCommentBranchesByBlogId_ExistingBlogWithSeveralLevelsOfNestingCommentBranches_ShouldReturnCommentsTree() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+
         // When
         List<CommentBranch> commentBranches = this.commentService.buildAllCommentBranchesByBlogId(1L);
 
@@ -171,6 +99,9 @@ public class CommentServiceTest {
 
     @Test
     public void buildAllCommentBranchesByBlogId_ExistingBlogWithoutComments_ShouldReturnEmptyCommentBranchesList() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+
         // When
         List<CommentBranch> commentBranches = this.commentService.buildAllCommentBranchesByBlogId(4L);
 
@@ -182,6 +113,8 @@ public class CommentServiceTest {
     public void buildAllCommentBranchesByBlogId_BlogCommentsBranchesWithSeveralSubBranches_ShouldSubmitNewComment() {
 
         // Given - before
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+
         List<BlogComment> blogCommentsBefore = this.commentDao.readAllByBlogId(1L);
         BlogComment newComment = this.generateBlogCommentInstance(1L, 1L, 2L);
 
@@ -199,31 +132,20 @@ public class CommentServiceTest {
 
     @Test
     public void buildLimitedCommentsBranchesUpToTripleNesting_TwoRootBranchesWithFourLevelsOfNesting_ShouldReturnTripleNestingCommentsTreeWithTheSpecifiedNumberOfComments() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
 
         // When
         List<CommentBranch> tripleNestingCommentsBranches = this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(1L, 1, 20);
 
         int totalAmount;
-        int rootSize = 0;
-        int firstSize = 0;
-        int secondSize = 0;
-        int thirdSize = 0;
 
-        if (tripleNestingCommentsBranches != null) {
-            rootSize = tripleNestingCommentsBranches.size();
-            for (CommentBranch rootLevelSubComment : tripleNestingCommentsBranches) {
-                List<CommentBranch> firstLevelSubComments = rootLevelSubComment.getSubComments();
-                firstSize += firstLevelSubComments.size();
-                for (CommentBranch firstLevelSubComment : firstLevelSubComments) {
-                    List<CommentBranch> secondLevel = firstLevelSubComment.getSubComments();
-                    secondSize += secondLevel.size();
-                    for (CommentBranch secondLevelSubComment : secondLevel) {
-                        List<CommentBranch> thirdLevel = secondLevelSubComment.getSubComments();
-                        thirdSize += thirdLevel.size();
-                    }
-                }
-            }
-        }
+        Map<Integer, Integer> subCommentsNumberForEachLevelOfNesting = this.countBranchSubCommentsNumberForEachLevelOfNesting(tripleNestingCommentsBranches);
+
+        int rootSize = subCommentsNumberForEachLevelOfNesting.get(0);
+        int firstSize = subCommentsNumberForEachLevelOfNesting.get(1);
+        int secondSize = subCommentsNumberForEachLevelOfNesting.get(2);
+        int thirdSize = subCommentsNumberForEachLevelOfNesting.get(3);
 
         totalAmount = rootSize + firstSize + secondSize;
 
@@ -295,26 +217,135 @@ public class CommentServiceTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void buildLimitedCommentsBranchesUpToTripleNesting_TwoRootBranchesWithFourLevelsOfNestingAndLinePerPageEqualZero_ShouldThrowException() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+        //When
         this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(1L, 1, 0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void buildLimitedCommentsBranchesUpToTripleNesting_TwoRootBranchesWithFourLevelsOfNestingAndPageEqualZero_ShouldThrowException() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+        // When
         this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(1L, 0, 10);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void buildLimitedCommentsBranchesUpToTripleNesting_BlogIdentifierEqualZero_ShouldThrowException() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+        // When
         this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(0L, 1, 10);
     }
 
     @Test
     public void buildLimitedCommentsBranchesUpToTripleNesting_NonExistentBlog_ShouldReturnEmptyCommentBranchesList() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(this.dataSource);
+
         // When
         List<CommentBranch> commentBranches = this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(1000L, 1, 10);
 
         // Then
         assertThat(commentBranches.size(), is(0));
+    }
+
+    @Test
+    public void buildLimitedCommentsBranchesUpToTripleNesting_BlogWithRootCommentBranchUpToOneLevelOfNesting_ShouldReturnSingleNestingCommentsTreeEqualOrLessTheSpecifiedNumberOfComments() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithRootCommentBranchUpToOneLevelOfNesting(this.dataSource);
+
+        // When
+        List<CommentBranch> commentBranches = this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(1L, 1, 10);
+
+        Map<Integer, Integer> subCommentsNumberForEachLevelOfNesting = this.countBranchSubCommentsNumberForEachLevelOfNesting(commentBranches);
+
+        int firstSize = subCommentsNumberForEachLevelOfNesting.get(1);
+        int secondSize = subCommentsNumberForEachLevelOfNesting.get(2);
+
+        CommentBranch commentBranch = commentBranches.get(0);
+
+        List<CommentBranch> firstLevelSubComments = commentBranch.getSubComments();
+
+       // Then
+        assertThat(commentBranches.size(), is(1));
+        assertThat(firstSize, is(4));
+        assertThat(secondSize, is(0));
+        assertThat(firstLevelSubComments.get(0).getRootBlogComment().getId(), is(5L));
+        assertThat(firstLevelSubComments.get(1).getRootBlogComment().getId(), is(4L));
+        assertThat(firstLevelSubComments.get(2).getRootBlogComment().getId(), is(3L));
+        assertThat(firstLevelSubComments.get(3).getRootBlogComment().getId(), is(2L));
+    }
+
+    @Test
+    public void buildLimitedCommentsBranchesUpToTripleNesting_BlogWithRootCommentBranchUpToOneDoubleNesting_ShouldReturnDoubleNestingCommentsTreeEqualOrLessTheSpecifiedNumberOfComments() {
+        // Given
+        GivenTestDataDistributor.prepareBlogWithRootCommentBranchUpToOneDoubleNesting(this.dataSource);
+
+        // When
+        List<CommentBranch> commentBranches = this.commentService.buildLimitedCommentsBranchesUpToTripleNesting(1L, 1, 10);
+
+        Map<Integer, Integer> subCommentsNumberForEachLevelOfNesting = this.countBranchSubCommentsNumberForEachLevelOfNesting(commentBranches);
+
+        int firstSize = subCommentsNumberForEachLevelOfNesting.get(1);
+        int secondSize = subCommentsNumberForEachLevelOfNesting.get(2);
+        int thirdSize = subCommentsNumberForEachLevelOfNesting.get(3);
+
+        CommentBranch commentBranch = commentBranches.get(0);
+
+        List<CommentBranch> firstLevelSubComments = commentBranch.getSubComments();
+
+        // Then
+        assertThat(commentBranches.size(), is(1));
+        assertThat(firstSize, is(2));
+        assertThat(secondSize, is(2));
+        assertThat(thirdSize, is(0));
+
+        CommentBranch firstLevelBranch1 = firstLevelSubComments.get(0);
+        CommentBranch firstLevelBranch2 = firstLevelSubComments.get(1);
+
+        assertThat(firstLevelBranch1.getRootBlogComment().getId(), is(3L));
+        assertThat(firstLevelBranch2.getRootBlogComment().getId(), is(2L));
+
+        List<CommentBranch> secondLevelSubComments1 = firstLevelBranch1.getSubComments();
+        List<CommentBranch> secondLevelSubComments2 = firstLevelBranch2.getSubComments();
+
+        assertThat(secondLevelSubComments1.get(0).getRootBlogComment().getId(), is(5L));
+        assertThat(secondLevelSubComments2.get(0).getRootBlogComment().getId(), is(4L));
+    }
+
+    private Map<Integer, Integer> countBranchSubCommentsNumberForEachLevelOfNesting(List<CommentBranch> commentBranches) {
+
+        Map<Integer, Integer> result = new HashMap<>();
+
+        int rootSize = 0;
+        int firstSize = 0;
+        int secondSize = 0;
+        int thirdSize = 0;
+
+        if (commentBranches != null) {
+            rootSize = commentBranches.size();
+            for (CommentBranch rootLevelSubComment : commentBranches) {
+                List<CommentBranch> firstLevelSubComments = rootLevelSubComment.getSubComments();
+                firstSize += firstLevelSubComments.size();
+                for (CommentBranch firstLevelSubComment : firstLevelSubComments) {
+                    List<CommentBranch> secondLevel = firstLevelSubComment.getSubComments();
+                    secondSize += secondLevel.size();
+                    for (CommentBranch secondLevelSubComment : secondLevel) {
+                        List<CommentBranch> thirdLevel = secondLevelSubComment.getSubComments();
+                        thirdSize += thirdLevel.size();
+                    }
+                }
+            }
+        }
+
+        result.put(0, rootSize);
+        result.put(1, firstSize);
+        result.put(2, secondSize);
+        result.put(3, thirdSize);
+
+        return result;
     }
 
     private BlogComment generateBlogCommentInstance(long blogId, long parentCommentId, long commentUserId) {
---
+++ blog-core/src/test/java/com/epam/adok/core/service/GivenTestDataDistributor.java
@@ -0,0 +1,116 @@
+package com.epam.adok.core.service;
+
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import com.ninja_squad.dbsetup.operation.Operation;
+
+import javax.sql.DataSource;
+
+import static com.ninja_squad.dbsetup.Operations.*;
+
+public class GivenTestDataDistributor {
+
+    public static void prepareBlogWithTwoFilledRootCommentBranchesAndBlogWithoutComments(DataSource dataSource) {
+
+        Operation operation;
+        operation = sequenceOf(
+                deleteAllFrom("blog_category_assignment", "comment", "blog"),
+                insertInto("blog")
+                        .columns("id", "title", "content", "user_id", "publication_date")
+                        .values(1L, "Blog#1", "Content Text", 1L, "2017-10-31 16:40:31")
+                        .values(4L, "Blog#2", "Content Text", 1L, "2017-10-31 16:40:31")
+                        .build(),
+                insertInto("comment")
+                        .columns("id", "blog_id", "parent_comment_id", "user_id", "text", "comment_date", "comment_type")
+                        .values(1L, 1L, null, 1L, "BlogComment Text id = 1", "2017-10-31 13:00:00", "BT")
+                        .values(2L, 1L, 1L, 2L, "BlogComment Text id = 2", "2017-10-31 13:30:00", "BT")
+                        .values(3L, 1L, 1L, 2L, "BlogComment Text id = 3", "2017-10-31 13:40:00", "BT")
+                        .values(4L, 1L, 3L, 1L, "BlogComment Text id = 4", "2017-10-31 13:45:00", "BT")
+                        .values(5L, 1L, 1L, 2L, "BlogComment Text id = 5", "2017-10-31 13:50:00", "BT")
+                        .values(6L, 1L, 5L, 1L, "BlogComment Text id = 6", "2017-10-31 14:00:00", "BT")
+                        .values(7L, 1L, 5L, 1L, "BlogComment Text id = 7", "2017-10-31 14:15:00", "BT")
+                        .values(8L, 1L, 5L, 1L, "BlogComment Text id = 8", "2017-10-31 14:20:00", "BT")
+                        .values(9L, 1L, 5L, 1L, "BlogComment Text id = 9", "2017-10-31 14:40:00", "BT")
+                        .values(10L, 1L, 3L, 1L, "BlogComment Text id = 10", "2017-10-31 14:55:00", "BT")
+                        .values(11L, 1L, 2L, 2L, "BlogComment Text id = 11", "2017-10-31 15:10:00", "BT")
+                        .values(12L, 1L, 1L, 1L, "BlogComment Text id = 12", "2017-10-31 15:40:00", "BT")
+                        .values(13L, 1L, 5L, 2L, "BlogComment Text id = 13", "2017-10-31 16:40:00", "BT")
+                        .values(14L, 1L, 6L, 1L, "BlogComment Text id = 14", "2017-10-31 16:50:00", "BT")
+                        .values(15L, 1L, 7L, 2L, "BlogComment Text id = 15", "2017-10-31 17:40:00", "BT")
+                        .values(16L, 1L, 3L, 1L, "BlogComment Text id = 16", "2017-10-31 17:50:00", "BT")
+                        .values(17L, 1L, 2L, 2L, "BlogComment Text id = 17", "2017-10-31 18:20:00", "BT")
+                        .values(18L, 1L, 3L, 1L, "BlogComment Text id = 18", "2017-10-31 18:25:00", "BT")
+                        .values(19L, 1L, 4L, 2L, "BlogComment Text id = 19", "2017-10-31 19:00:00", "BT")
+                        .values(20L, 1L, 6L, 1L, "BlogComment Text id = 20", "2017-10-31 19:20:00", "BT")
+                        .values(21L, 1L, 1L, 2L, "BlogComment Text id = 21", "2017-10-31 19:30:00", "BT")
+                        .values(22L, 1L, 2L, 1L, "BlogComment Text id = 22", "2017-10-31 19:40:00", "BT")
+                        .values(23L, 1L, null, 1L, "BlogComment Text id = 23", "2017-10-31 13:20:00", "BT")
+                        .values(24L, 1L, 23L, 2L, "BlogComment Text id = 24", "2017-10-31 13:35:00", "BT")
+                        .values(25L, 1L, 24L, 2L, "BlogComment Text id = 25", "2017-10-31 19:45:00", "BT")
+                        .values(26L, 1L, 24L, 1L, "BlogComment Text id = 26", "2017-10-31 19:45:00", "BT")
+                        .values(27L, 1L, 23L, 2L, "BlogComment Text id = 27", "2017-10-31 13:55:00", "BT")
+                        .values(28L, 1L, 26L, 1L, "BlogComment Text id = 28", "2017-10-31 14:30:00", "BT")
+                        .values(29L, 1L, 27L, 1L, "BlogComment Text id = 29", "2017-10-31 19:20:00", "BT")
+                        .values(30L, 1L, 29L, 1L, "BlogComment Text id = 30", "2017-10-31 14:20:50", "BT")
+                        .values(31L, 1L, 28L, 1L, "BlogComment Text id = 31", "2017-10-31 14:40:40", "BT")
+                        .values(32L, 1L, 26L, 1L, "BlogComment Text id = 32", "2017-10-31 14:55:20", "BT")
+                        .values(33L, 1L, 29L, 2L, "BlogComment Text id = 33", "2017-10-31 15:10:30", "BT")
+                        .values(34L, 1L, 30L, 1L, "BlogComment Text id = 34", "2017-10-31 15:40:50", "BT")
+                        .values(35L, 1L, 31L, 2L, "BlogComment Text id = 35", "2017-10-31 16:40:30", "BT")
+                        .values(36L, 1L, 33L, 1L, "BlogComment Text id = 36", "2017-10-31 16:50:20", "BT")
+                        .values(37L, 1L, 33L, 2L, "BlogComment Text id = 37", "2017-10-31 17:40:30", "BT")
+                        .values(38L, 1L, 25L, 1L, "BlogComment Text id = 38", "2017-10-31 17:50:40", "BT")
+                        .values(39L, 1L, 26L, 2L, "BlogComment Text id = 39", "2017-10-31 18:20:50", "BT")
+                        .values(40L, 1L, 32L, 1L, "BlogComment Text id = 40", "2017-10-31 18:25:30", "BT")
+                        .values(41L, 1L, 33L, 2L, "BlogComment Text id = 41", "2017-10-31 19:00:50", "BT")
+                        .values(42L, 1L, 31L, 1L, "BlogComment Text id = 42", "2017-10-31 19:20:30", "BT")
+                        .values(43L, 1L, 34L, 2L, "BlogComment Text id = 43", "2017-10-31 19:30:50", "BT")
+                        .values(44L, 1L, 37L, 1L, "BlogComment Text id = 44", "2017-10-31 19:45:20", "BT")
+                        .build());
+        DbSetup dbSetup = new DbSetup(new DataSourceDestination(dataSource), operation);
+        dbSetup.launch();
+    }
+
+    public static void prepareBlogWithRootCommentBranchUpToOneLevelOfNesting(DataSource dataSource) {
+
+        Operation operation;
+        operation = sequenceOf(
+                deleteAllFrom("blog_category_assignment", "comment", "blog"),
+                insertInto("blog")
+                        .columns("id", "title", "content", "user_id", "publication_date")
+                        .values(1L, "Blog#1", "Content Text", 1L, "2017-10-31 16:40:31")
+                        .build(),
+                insertInto("comment")
+                        .columns("id", "blog_id", "parent_comment_id", "user_id", "text", "comment_date", "comment_type")
+                        .values(1L, 1L, null, 1L, "BlogComment Text id = 1", "2017-10-31 13:00:00", "BT")
+                        .values(2L, 1L, 1L, 2L, "BlogComment Text id = 2", "2017-10-31 13:30:10", "BT")
+                        .values(3L, 1L, 1L, 2L, "BlogComment Text id = 3", "2017-10-31 13:30:20", "BT")
+                        .values(4L, 1L, 1L, 2L, "BlogComment Text id = 4", "2017-10-31 13:30:30", "BT")
+                        .values(5L, 1L, 1L, 2L, "BlogComment Text id = 5", "2017-10-31 13:30:40", "BT")
+                        .build());
+        DbSetup dbSetup = new DbSetup(new DataSourceDestination(dataSource), operation);
+        dbSetup.launch();
+    }
+
+    public static void prepareBlogWithRootCommentBranchUpToOneDoubleNesting(DataSource dataSource) {
+
+        Operation operation;
+        operation = sequenceOf(
+                deleteAllFrom("blog_category_assignment", "comment", "blog"),
+                insertInto("blog")
+                        .columns("id", "title", "content", "user_id", "publication_date")
+                        .values(1L, "Blog#1", "Content Text", 1L, "2017-10-31 16:40:31")
+                        .build(),
+                insertInto("comment")
+                        .columns("id", "blog_id", "parent_comment_id", "user_id", "text", "comment_date", "comment_type")
+                        .values(1L, 1L, null, 1L, "BlogComment Text id = 1", "2017-10-31 13:00:00", "BT")
+                        .values(2L, 1L, 1L, 2L, "BlogComment Text id = 2", "2017-10-31 13:30:10", "BT")
+                        .values(3L, 1L, 1L, 2L, "BlogComment Text id = 3", "2017-10-31 13:30:40", "BT")
+                        .values(4L, 1L, 2L, 2L, "BlogComment Text id = 4", "2017-10-31 13:30:40", "BT")
+                        .values(5L, 1L, 3L, 2L, "BlogComment Text id = 5", "2017-10-31 13:30:40", "BT")
+                        .build());
+        DbSetup dbSetup = new DbSetup(new DataSourceDestination(dataSource), operation);
+        dbSetup.launch();
+    }
+
+}
